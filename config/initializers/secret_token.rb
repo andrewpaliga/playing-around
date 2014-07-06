af0c1b4ae70445fa1c0ff0771a0e3bb206db952a
Andrew P <andrew.paliga@jadexpixel.com>
2014-07-06 10:03:14 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlayingAround::Application.config.secret_key_base = '6fc6ac01f2eeb1602e21bfb31edff6e1ef2c405ee4cce6fc835295f61d59c43e47a23364a5ef59ccbbee2ecf9d2c04a2a608217a14097290f050e70662ff4d2d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PlayingAround::Application.config.session_store :cookie_store, key: '_playing-around_session'
